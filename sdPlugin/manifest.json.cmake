553505e3ba67b5ab9850ad892c4db6a765630b71
Fred Emmott <git@fredemmott.com>
2020-06-01 08:46:53 -0700

    dos2unix on manifest, format

--- sdPlugin/manifest.json.cmake
+++ sdPlugin/manifest.json.cmake
@@ -5,36 +5,35 @@
         {
           "Image": "headphones"
         },
-		{
+        {
           "Image": "speakers"
         }
       ],
       "SupportedInMultiActions": false,
-	  "Icon": "headphones",
-	  "Name": "Toggle Audio Device",
+      "Icon": "headphones",
+      "Name": "Toggle Audio Device",
       "Tooltip": "Switch between two audio devices.",
       "UUID": "com.fredemmott.audiooutputswitch.toggle"
     },
-	{
-		"SupportedInMultiActions": true,
-		"Icon": "active",
-		"Name": "Set Audio Device",
-		"Tooltip": "Set a specific audio device",
-		"UUID": "com.fredemmott.audiooutputswitch.set",
-		"States": [
-			{
-				"Image": "active"
-			},
-			{
-				"Image": "inactive"
-			}
-		]
-	}
+    {
+      "SupportedInMultiActions": true,
+      "Icon": "active",
+      "Name": "Set Audio Device",
+      "Tooltip": "Set a specific audio device",
+      "UUID": "com.fredemmott.audiooutputswitch.set",
+      "States": [
+        {
+          "Image": "active"
+        },
+        {
+          "Image": "inactive"
+        }
+      ]
+    }
   ],
   "Author": "Fred Emmott",
   "CodePath": "sdaudioswitch.exe",
-    "CodePathWin": "sdaudioswitch.exe",
-
+  "CodePathWin": "sdaudioswitch.exe",
   "Description": "Toggle or set the active audio devices.",
   "Name": "Audio Output",
   "PropertyInspectorPath": "propertyinspector/index.html",
@@ -42,12 +41,12 @@
   "Version": "${CMAKE_PROJECT_VERSION}",
   "OS": [
     {
-        "Platform": "windows",
-        "MinimumVersion" : "10"
+      "Platform": "windows",
+      "MinimumVersion": "10"
     }
   ],
   "SDKVersion": 2,
   "Software": {
-    "MinimumVersion" : "4.1"
+    "MinimumVersion": "4.1"
   }
 }
