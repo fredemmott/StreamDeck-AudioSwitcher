477082347755befbb1983febdd583d17d08c9472
Fred Emmott <git@fredemmott.com>
2022-09-17 11:44:53 -0500

    Update AudioDeviceLib

--- AudioDeviceLib.cmake
+++ AudioDeviceLib.cmake
@@ -2,8 +2,8 @@ include(FetchContent)
 
 FetchContent_Declare(
   AudioDeviceLib
-  URL https://github.com/fredemmott/AudioDeviceLib/releases/download/v2.1.0/AudioDeviceLib-v2.1.0.zip
-  URL_HASH SHA512=ecd4be984caaee11af199c6d5eb02986c53908ce8ee60ebb83ce193182a94109f28742335f6d734110d583dd0e5c7f01a9a9fa54f615d9f3e351163c2ad7dce5
+  URL https://github.com/fredemmott/AudioDeviceLib/releases/download/v2.1.1/AudioDeviceLib-v2.1.1.zip
+  URL_HASH SHA512=7f59d62a094eb140008f7d9d6ab72b2a7a360c23a5dee47f533c96ae3e65762acc6e927a8fccbad5adfc247790fc7bb92a3ff204c1b48fefb7a6f3df8243d5fa
 )
 
 FetchContent_GetProperties(AudioDeviceLib)
